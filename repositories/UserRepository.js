ea8102bca973af62d23c2a238cc949b960408c24
Will Aronow <waronow123@gmail.com>
2021-12-15 20:21:37 -0700

    added ID generation, find,update,delete methods

--- repositories/UserRepository.js
+++ repositories/UserRepository.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const crypto = require('crypto');
+
 class UserRepository {
 	constructor(filename) {
 		if (!filename) throw new Error('Filename required to create repository');
@@ -12,12 +14,32 @@ class UserRepository {
 	async getAll() {
 		return JSON.parse(await fs.promises.readFile(this.filename, { encoding: 'utf8' }));
 	}
+	async getOne(id) {
+		const users = await this.getAll();
+		return users.find((user) => user.id === id);
+	}
 	async create(attrs) {
 		const data = await this.getAll();
+		attrs.id = this.createRandomId();
 		data.push(attrs);
 		await this.writeAll(data);
 	}
+	async update(id, attrs) {
+		const users = await this.getAll();
+		const user = users.find((user) => user.id === id);
+		if (!user) throw new Error(`UserID ${id} not found`);
+		Object.assign(user, attrs);
+		await this.writeAll(users);
+	}
+	async delete(id) {
+		const users = await this.getAll();
+		const filtered = users.filter((user) => user.id !== id);
+		await this.writeAll(filtered);
+	}
 	async writeAll(info) {
 		await fs.promises.writeFile(this.filename, JSON.stringify(info, null, 2));
+	}
+	createRandomId() {
+		return crypto.randomBytes(4).toString('hex');
 	}
 }
