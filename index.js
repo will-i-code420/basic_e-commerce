b58c20866976babd0e2de91a3e9b54b2905a3f31
Will Aronow <waronow123@gmail.com>
2021-12-16 15:56:22 -0700

    created signin/out routes

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ app.use(
 	})
 );
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
 	res.send(`
     <div>
     <form method="POST">
     <input type="email" name="email" placeholder="email"/>
     <input type="password" name="password" placeholder="password"/>
     <input type="password" name="confirm" placeholder="confirm password"/>
-    <button>Register</button>
+    <button>Sign Up</button>
     </form>
     </div>
     `);
 });
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
 	const { email, password, confirm } = req.body;
 	const existingUser = await userRepo.getOne({ email });
 	if (existingUser) {
@@ -36,6 +36,33 @@ app.post('/', async (req, res) => {
 	const user = await userRepo.create({ email, password });
 	req.session.userId = user.id;
 	res.send('Account Created');
+});
+
+app.get('/signout', (req, res) => {
+	req.session = null;
+	res.redirect('/signin');
+});
+
+app.get('/signin', (req, res) => {
+	res.send(`
+	<div>
+    <form method="POST">
+    <input type="email" name="email" placeholder="email"/>
+    <input type="password" name="password" placeholder="password"/>
+    <button>Sign In</button>
+    </form>
+    </div>
+	`);
+});
+
+app.post('/signin', async (req, res) => {
+	const { email, password } = req.body;
+	const user = userRepo.getOne({ email });
+	if (!user || user.password !== password) {
+		return res.send('Incorrect email or password');
+	}
+	req.session.userId = user.id;
+	res.send('sign ins');
 });
 
 app.listen(port, () => {
