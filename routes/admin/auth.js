774f0ced66385cc63fb9b2372f8f4c408672bf3a
Will Aronow <waronow123@gmail.com>
2021-12-18 21:10:11 -0700

    added form validation with express-validator

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 const userRepo = require('../../repositories/UserRepository');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -8,19 +9,41 @@ router.get('/signup', (req, res) => {
 	res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
-	const { email, password, confirm } = req.body;
-	const existingUser = await userRepo.getOne({ email });
-	if (existingUser) {
-		return res.send('email already registered');
+router.post(
+	'/signup',
+	[
+		check('email')
+			.trim()
+			.normalizeEmail()
+			.isEmail()
+			.withMessage('Must be valid email address')
+			.custom(async (email) => {
+				const existingUser = await userRepo.getOne({ email });
+				if (existingUser) throw new Error('email already registered');
+			}),
+		check('password').trim().isLength({ min: 4, max: 32 }).withMessage('Must be between 4 and 32 characters'),
+		check('confirm')
+			.trim()
+			.isLength({ min: 4, max: 32 })
+			.withMessage('Must be between 4 and 32 characters')
+			.custom((confirm, { req }) => {
+				if (confirm !== req.body.password) throw new Error('Password does not match confirmation');
+			})
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (errors) {
+		}
+		const { email, password, confirm } = req.body;
+
+		if (password !== confirm) {
+			return res.send('Password does not match confirmation');
+		}
+		const user = await userRepo.create({ email, password });
+		req.session.userId = user.id;
+		res.send('Account Created');
 	}
-	if (password !== confirm) {
-		return res.send('Password does not match confirmation');
-	}
-	const user = await userRepo.create({ email, password });
-	req.session.userId = user.id;
-	res.send('Account Created');
-});
+);
 
 router.get('/signout', (req, res) => {
 	req.session = null;
